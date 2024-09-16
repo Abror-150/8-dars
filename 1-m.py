c63c7861136f307772e3c4b970c4e275607624bb
Abror-150 <urazalievv.abror@gmail.com>
2024-09-16 09:17:55 +0500

    Add files via upload

---
+++ 1-m.py
@@ -0,0 +1,11 @@
+f = open("main/7-dars/file.vazifa/input.txt","r")
+file = open("main/7-dars/file.vazifa/a.file.txt","w")
+
+son = f.read().split()
+for i in son:
+    i = int(i)
+    
+    file.write(chr(i))
+
+
+
